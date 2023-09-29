1a60fda988c904d4cadde83ce52a0dee22ac41f2
Markswell <osoromarkswell80@gmail.com>
2023-09-29 09:14:26 +0300

    Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-const init = () => {
-  
-}
+// const init = () => {
+//     const inputForm = document.querySelector('form');
+//     inputForm.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//        const input = document.querySelector('input#searchByID');
+//         e.target.children[1].value
 
-document.addEventListener('DOMContentLoaded', init);
+//         console.log(input.value);
+
+//         fetch('http://localhost:3000/movies')
+//         .then(response => response.json())
+//         .then(data => {
+//         console.log(data);
+//     });
+// }}
+// document.addEventListener('DOMContentLoaded', init);
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      console.log(input.value);
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
